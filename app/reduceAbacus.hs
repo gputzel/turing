7797d987522faa39c2954109925c96dd5c096394
Gregory Putzel <gregputzel@gmail.com>
2025-10-10 20:24:42 -0400

    Make function that generates Turing map for an arbitrary register incrementer

--- app/reduceAbacus.hs
+++ app/reduceAbacus.hs
@@ -27,14 +27,16 @@ opts = Opt.info (options <**> Opt.helper)
    <> Opt.progDesc "Reduce an abacus machine to a Turing machine"
    <> Opt.header "abacusReduce - abacus machine reducer" )
 
+
 main :: IO ()
 main = do
     Options { optAbacusMapFile = abacusMapFile } <- Opt.execParser opts    
     abacusInput <- readFile abacusMapFile
-    let tokens = alexScanTokens abacusInput
-    let amap = parseAbacus tokens
-    let incrementer_transitional_block_renamed = prependToStateNames "s2" incrementer_transitional_block
-    let incrementer_begin = stitch incrementer_initial_block incrementer_transitional_block_renamed (HeadState "s2a")
-    putStrLn $ show incrementer_initial_block
-    putStrLn $ show incrementer_transitional_block
-    putStrLn $ show incrementer_begin
+    let
+        tokens = alexScanTokens abacusInput
+        amap = parseAbacus tokens
+        incrementer_transitional_block_renamed = prependToStateNames "s2" incrementer_transitional_block
+        incrementer_begin = stitch incrementer_initial_block incrementer_transitional_block_renamed (HeadState "s2a")
+        incrementer = stitch incrementer_begin incrementer_end (HeadState "lower_left_of_square")
+        other_incrementer = make_incrementer 4
+    printMap other_incrementer
--- src/Abacus2Turing.hs
+++ src/Abacus2Turing.hs
@@ -4,6 +4,17 @@ import TuringMachine.Types
 import qualified Data.Map as Map
 import Data.Map (Map)
 
+printMap :: TransitionMap -> IO ()
+printMap tMap = printList (Map.toList tMap) where
+    printList [] = return () 
+    printList (p:l) =
+        do
+            let ((tSym1,hState1),(tSym2,hMove,hState2)) = p
+            let lhs = (show tSym1) ++ " " ++ (show hState1)
+            let rhs = (show tSym2) ++ " " ++ (show hMove) ++ " " ++ (show hState2)
+            putStrLn $ lhs ++ " -> " ++ rhs
+            printList l
+
 --Note that we need to do prepending in both the keys and values
 prependToStateNames :: String -> TransitionMap -> TransitionMap
 prependToStateNames prefix = (Map.map value_prep) . (Map.mapKeys key_prep) where
@@ -37,4 +48,46 @@ incrementer_transitional_block = Map.fromList [
                                     ,((Blank,HeadState "b"),(Dash,Stay,Halt))
                                 ]
 
+incrementer_end :: TransitionMap
+incrementer_end = Map.fromList [
+        ((Dash,HeadState "lower_left_of_square"),(Dash,JumpRight,HeadState "lower_right_of_square"))
+        ,((Blank,HeadState "lower_left_of_square"),(Blank,Stay,HeadState "error"))
+        ,((Dash,HeadState "lower_right_of_square"),(Blank,Stay,HeadState "upper_right_of_square"))
+        ,((Blank,HeadState "lower_right_of_square"),(Blank,JumpLeft,HeadState "left_loop"))
+        ,((Dash,HeadState "upper_right_of_square"),(Dash,Stay,HeadState "error"))
+        ,((Blank,HeadState "upper_right_of_square"),(Blank,JumpRight,HeadState "upper_left_of_square"))
+        ,((Dash,HeadState "upper_left_of_square"),(Dash,JumpRight,HeadState "upper_left_of_square"))
+        ,((Blank,HeadState "upper_left_of_square"),(Dash,Stay,HeadState "lower_left_of_square"))
+        ,((Dash,HeadState "left_loop"),(Dash,JumpLeft,HeadState "left_loop"))
+        ,((Blank,HeadState "left_loop"),(Blank,JumpLeft,HeadState "second_to_last"))
+        ,((Dash,HeadState "second_to_last"),(Dash,JumpLeft,HeadState "left_loop"))
+        ,((Blank,HeadState "second_to_last"),(Blank,JumpRight,HeadState "last"))
+        ,((Dash,HeadState "last"),(Dash,Stay,HeadState "error"))
+        ,((Blank,HeadState "last"),(Blank,JumpRight,Halt))
+    ]
 
+--This is a helper function for stitching together lists
+--of transition maps. For each one, we need to know what prefix
+--to prepend to the state names, and also the HeadState that we
+--need to wire Halt into
+stitchList :: [(TransitionMap,String,HeadState)] -> TransitionMap
+stitchList [] = Map.fromList []
+stitchList ((tMap,prefix,hState):l) = stitch leftMap rightMap hState
+    where
+        leftMap = prependToStateNames prefix tMap
+        rightMap = stitchList l 
+
+make_incrementer :: Int -> TransitionMap
+--make_incrementer :: Int -> [(String,HeadState)]
+make_incrementer n = stitchList $ zip3 tMaps prefixes hStates
+--make_incrementer n = zip prefixes hStates
+    where
+        tMaps = initialBlocks ++ [incrementer_transitional_block,incrementer_end]
+        initialBlocks = take (n-1) $ repeat incrementer_initial_block
+        prefixes = initialPrefixes ++ ["s" ++ (show n),""]
+        initialPrefixes = take (n-1) (zipWith (++) s int_strs_start_1)
+        int_strs_start_1 = map show [1..]
+        hStates = initialStates ++ [HeadState "lower_left_of_square",Halt]
+        initialStates = take (n-1) $ zipWith zipper s [2..]
+        zipper str i = HeadState (str ++ (show i) ++ "a")
+        s = repeat "s"
